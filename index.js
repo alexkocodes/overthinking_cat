d440479d04ffed3d8633d09f864594b47a1ba1c1
Alex Ko <fyk211@nyu.edu>
2022-03-08 01:25:59 +0400

    frame2

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ function typewriter()
     }
     iIndex=0;
 }
+
 typewriter();
+setInterval(typewriter, 8000);
+
 
 Number.prototype.map = function (in_min, in_max, out_min, out_max) {
     return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
