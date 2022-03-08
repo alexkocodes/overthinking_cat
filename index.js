c887c3b07e7c271e65abbbbac45a830c303c1b83
Ngoc N. Hoang <nnh245@nyu.edu>
2022-03-08 12:17:13 +0400

    Update backgrounds

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function change_thought_size(){
 
 setInterval(change_thought_size, 10);
 
-scenario1 = false; // change here
+scenario1 = true; // change here
 var case1 = document.getElementsByClassName('scenario1');
 var case2 = document.getElementsByClassName('scenario2');
 if(scenario1===true){
